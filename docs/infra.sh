00cebc9b512d6c255fe6f02a6d4500ad8fb3187e
jobscale <jobscale@users.noreply.github.com>
2021-07-22 15:16:09 +0900

    maintenance updated

--- docs/infra.sh
+++ docs/infra.sh
@@ -2,6 +2,6 @@ if [[ $(grep -i debian /etc/*release | wc -l) > 0 ]]
 then
   export DEBIAN_FRONTEND=noninteractive
   apt update
-  apt install -y net-tools dnsutils iputils-ping procps sudo vim curl git tmux
+  apt install -y iproute2 dnsutils iputils-ping procps sudo vim curl git tmux
   rm -fr /var/lib/apt/lists/*
 fi
